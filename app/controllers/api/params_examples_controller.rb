d78df6fc45a212df285567091706ee00f9a4b1f6
Peter Jang <peter.x.jang@gmail.com>
2018-10-14 09:35:26 -0500

    Add query params example

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -1,5 +1,7 @@
 class Api::ParamsExamplesController < ApplicationController
   def query_params_method
+    @the_name = params["name"]
+    @my_price = params["price"]
     render "query_params.json.jbuilder"
   end
 end
--- app/views/api/params_examples/query_params.json.jbuilder
+++ app/views/api/params_examples/query_params.json.jbuilder
@@ -1,1 +1,3 @@
 json.message "Hi"
+json.extra_info @the_name
+json.money @my_price
