c1bf542b48e16b4f425f6808923ab51cb3e64c60
Peter Jang <peter.x.jang@gmail.com>
2018-10-15 18:47:40 -0500

    Add body params number guess example

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     get "/query_guess_url" => "params_examples#query_guess_method"
 
     get "/segment_guess_url/:guess" => "params_examples#query_guess_method"
+
+    post "/body_guess_url" => "params_examples#query_guess_method"
   end
 end
